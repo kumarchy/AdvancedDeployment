040625572d92d1f6c4491585f0a6addf6c88109a
Kumar Chaudhary <Kumar.Chaudhary@valuehealthai.com>
2025-10-30 12:18:32 +0530

    updated

--- scripts/find-changes.bat
+++ scripts/find-changes.bat
@@ -23,6 +23,7 @@ git diff --name-only HEAD~1 HEAD > "%DELTA_DIR%\changed_files.txt"
 
 echo Changed files:
 type "%DELTA_DIR%\changed_files.txt"
+echo.
 
 for /f "usebackq delims=" %%f in ("%DELTA_DIR%\changed_files.txt") do (
     set FILE=%%f
@@ -31,51 +32,49 @@ for /f "usebackq delims=" %%f in ("%DELTA_DIR%\changed_files.txt") do (
     ) else (
         if exist "!FILE!" (
             echo Copying !FILE! ...
-        
-        REM --- Apex Classes ---
-        if "!FILE:force-app/main/default/classes=!" neq "!FILE!" (
+
+            REM --- Apex Classes ---
+            if "!FILE:force-app/main/default/classes=!" neq "!FILE!" (
                 mkdir "%DELTA_DIR%\src\classes" >nul 2>&1
                 copy "!FILE!" "%DELTA_DIR%\src\classes\" >nul
             )
 
-        REM --- Apex Triggers ---
-        if "!FILE:force-app/main/default/triggers=!" neq "!FILE!" (
+            REM --- Apex Triggers ---
+            if "!FILE:force-app/main/default/triggers=!" neq "!FILE!" (
                 mkdir "%DELTA_DIR%\src\triggers" >nul 2>&1
                 copy "!FILE!" "%DELTA_DIR%\src\triggers\" >nul
             )
-        
-        REM --- Lightning Web Components ---
-        if "!FILE:force-app/main/default/lwc=!" neq "!FILE!" (
-        for %%a in ("!FILE!") do (
-        set "COMPONENT=%%~dpa"
-        set "COMPONENT=!COMPONENT:~0,-1!"
-        for %%b in ("!COMPONENT!") do (
-            xcopy "%%b" "%DELTA_DIR%\src\lwc\%%~nxb" /E /I /Y >nul
+
+            REM --- Lightning Web Components ---
+            if "!FILE:force-app/main/default/lwc=!" neq "!FILE!" (
+                for %%a in ("!FILE!") do (
+                    set "COMPONENT_PATH=%%~dpa"
+                    for %%b in ("!COMPONENT_PATH:~0,-1!") do (
+                        xcopy "%%b" "%DELTA_DIR%\src\lwc\%%~nxb" /E /I /Y >nul
+                    )
+                )
+            )
         )
     )
 )
 
-        )
-    )
-)
-
-REM Generate simple package.xml
+REM --- Generate Simple package.xml ---
 (
 echo ^<?xml version="1.0" encoding="UTF-8"?^>
 echo ^<Package xmlns="http://soap.sforce.com/2006/04/metadata"^>
-echo ^<types^>
-echo     ^<members^>*^</members^>
-echo     ^<name^>ApexClass^</name^>
-echo ^</types^>
-echo ^<types^>
-echo     ^<members^>*^</members^>
-echo     ^<name^>ApexTrigger^</name^>
-echo ^</types^>
-echo ^<types^>
-echo     ^<members^>*^</members^>
-echo     ^<name^>LightningComponentBundle^</name^>
-echo ^</types^>
-echo ^<version^>60.0^</version^>
+echo     ^<types^>
+echo         ^<members^>*^</members^>
+echo         ^<name^>ApexClass^</name^>
+echo     ^</types^>
+echo     ^<types^>
+echo         ^<members^>*^</members^>
+echo         ^<name^>ApexTrigger^</name^>
+echo     ^</types^>
+echo     ^<types^>
+echo         ^<members^>*^</members^>
+echo         ^<name^>LightningComponentBundle^</name^>
+echo     ^</types^>
+echo     ^<version^>60.0^</version^>
 echo ^</Package^>
 ) > "%DELTA_DIR%\package.xml"
 
