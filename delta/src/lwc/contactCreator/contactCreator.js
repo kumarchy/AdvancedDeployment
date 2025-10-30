3a289e0611a2d9c3e2344ebdd57675e33137eb49
Kumar Chaudhary <Kumar.Chaudhary@valuehealthai.com>
2025-10-30 14:49:59 +0530

    update

---
+++ delta/src/lwc/contactCreator/contactCreator.js
@@ -0,0 +1,30 @@
+import { LightningElement, track } from 'lwc';
+import createContact from '@salesforce/apex/ContactHandler.createContact';
+
+export default class ContactCreator extends LightningElement {
+    @track firstName = '';
+    @track lastName = '';
+    @track accountId = '';
+    @track message = '';
+
+    handleChange(event) {
+        const label = event.target.label;
+        if (label === 'First Name') this.firstName = event.target.value;
+        else if (label === 'Last Name') this.lastName = event.target.value;
+        else if (label === 'Account Id') this.accountId = event.target.value;
+    }
+
+    async createContact() {
+        try {
+            const result = await createContact({ 
+                firstName: this.firstName, 
+                lastName: this.lastName, 
+                accountId: this.accountId 
+            });
+            this.message = result;
+            console.log(this.message);
+        } catch (error) {
+            this.message = 'Error: ' + error.body.message;
+        }
+    }
+}
